11641d10337191cdefd7be7b65482802ed5556d2
Randy <dddrandy@gmail.com>
2025-09-23 17:29:37 +0930

    Chore: remove unused imports

--- frontend/src/pages/EventsList.tsx
+++ frontend/src/pages/EventsList.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useSearchParams } from "react-router-dom";
 import { api, type EventDto } from "../lib/api";
 
